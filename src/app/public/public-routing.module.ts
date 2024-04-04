9d7e7f06f97b223892426c04ea75adf3f16af473
mdmuneeb <muneebzafar999@gmail.com>
2024-04-05 03:33:00 +0500

    Landing Page updated

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {path:"",component:PublicLayoutComponent,children:[
     {path:"",component:HomeComponent},
-    {path:"login",component:LoginComponent}
+    {path:"login",component:LoginComponent},
+    {path: "about-us", component: AboutUsComponent},
+    {path: "services", component: ServicesComponent}
   ]},
  
 ];
