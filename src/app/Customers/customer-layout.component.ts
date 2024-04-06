c3d70360f2dc7d23c4f8d5c74e67b0e9aa2a2f8c
MuhammadMohsinShaikh <119584309+MMohsin04@users.noreply.github.com>
2024-04-07 03:17:59 +0500

    Add Dialog for Add File

--- src/app/Customers/customer-layout.component.ts
+++ src/app/Customers/customer-layout.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UploadShipmentsComponent } from './upload-shipments/upload-shipments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 @Component({
   selector: 'app-customer-layout',
   templateUrl: './customer-layout.component.html',
-  styleUrls: ['./customer-layout.component.scss']
+  styleUrls: ['./customer-layout.component.scss'],
+  providers:[DialogService]
 })
 
 
 export class CustomerLayoutComponent {
-  constructor(private router:Router,private dialog : MatDialog) { }
-  Logout(){
+  ref: DynamicDialogRef | undefined;
+  constructor(private router: Router,
+    public dialogService: DialogService
+  ) { }
+  Logout() {
     localStorage.removeItem("login")
     this.router.navigate(['/'])
   }
 
-  openUpload(){
-    const dialogRef = this.dialog.open(UploadShipmentsComponent, {
-      width: "45%",
-    })}
+  openUpload() {
+    this.ref = this.dialogService.open(UploadShipmentsComponent, {
+      header: 'Add File',
+      width: '50vw',
+      modal: true,
+      breakpoints: {
+        '960px': '75vw',
+        '640px': '90vw'
+      },
+    })
+  }
+
 }
